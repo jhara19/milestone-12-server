2e6551b23ed5e1c4b9b4fa1db9d8035eef68e279
jhara19 <jharaislam639@gmail.com>
2022-11-16 13:24:38 +0600

    first commit

--- index.js
+++ index.js
@@ -20,18 +20,45 @@ app.get('/', (req, res) => {
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jj8cffr.mongodb.net/?retryWrites=true&w=majority`;
-//console.log(uri);
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(params) {
    try{
     const appointmentOptions = client.db('doctors-portal').collection('appointments');
+    const bookingCollection = client.db('doctors-portal').collection('bookings');
+
+
+    //Use aggrigate to query multiple collection and then merge data
 
     app.get('/appointments', async (req,res) => {
+        const date = req.query.date;
+        console.log(date);
+        //=======
         const query ={};
         const options = await appointmentOptions.find(query).toArray();
+        //===================
+//code carefully
+        const bookingQuery = {appointmentDate:date};
+        const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+        options.forEach(option => {
+            const optionBooked = alreadyBooked.filter(book => book.treatment == option.name);
+            const bookedSlots = optionBooked.map(book => book.slot)
+            console.log(date,option.name,bookedSlots);
+        })
         res.send(options);
-    })
+    });
+
+//booking post
+app.post('/bookings', async (req,res) => {
+    const booking = req.body;
+    console.log(booking);
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
+})
+
+
+
    }
    finally{
 
